93502d9e66136e7753ba00425045713fd0235ac8
chokkadevops <chokka26a@gmail.com>
2026-07-04 12:10:37 +0530

    testing github jenkins automation scenario

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def log_env_and_db():
 
 @app.route("/")
 def index():
-    return "Testing webhook automate process.."
+    return "Testing Github Jenkins automation process at 12:10.."
 
 if __name__ == "__main__":
     threading.Thread(target=log_env_and_db, daemon=True).start()
