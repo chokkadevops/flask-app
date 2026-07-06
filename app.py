85deea3b844947803c9da80bde094f4c892ec80f
chokkadevops <chokka26a@gmail.com>
2026-07-06 09:55:53 +0530

    Preparing for Jenkins Automate Demo

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def log_env_and_db():
 
 @app.route("/")
 def index():
-    return "Preparing Jenkins automation demo.."
+    return "July 06,2026 Monday morning demo.."
 
 if __name__ == "__main__":
     threading.Thread(target=log_env_and_db, daemon=True).start()
