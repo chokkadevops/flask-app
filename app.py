d1d1cdcfae85ae9ebc97e9a251067ba0e81bf602
chokkadevops <chokka26a@gmail.com>
2026-07-07 11:00:31 +0530

    Changes to app file for Jenkins automation

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def log_env_and_db():
 
 @app.route("/")
 def index():
-    return "Testing the webhook.."
+    return "Testing the Git automation.."
 
 if __name__ == "__main__":
     threading.Thread(target=log_env_and_db, daemon=True).start()
